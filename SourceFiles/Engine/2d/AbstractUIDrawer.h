36d96cd47eeea795a0885be0541b5d52e558d1c4
SyuyaNakayama <k021g1352@g.neec.ac.jp>
2024-05-03 15:30:55 +0900

    AbstractUIDrawerを変更

--- SourceFiles/Engine/2d/AbstractUIDrawer.h
+++ SourceFiles/Engine/2d/AbstractUIDrawer.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Constant.h"
+#include "Sprite.h"
+#include <map>
 
 namespace WristerEngine::_2D
 {
@@ -9,15 +11,16 @@ namespace WristerEngine::_2D
 	{
 	protected:
 		Constant* constant = Constant::GetInstance();
+		std::map<std::string, std::unique_ptr<Sprite>> sprites;
 
 	public:
 		// 仮想デストラクタ
 		virtual ~AbstractUIDrawer() = default;
 		// 初期化(純粋仮想関数)
 		virtual void Initialize() = 0;
-		// 更新(純粋仮想関数)
-		virtual void Update() = 0;
-		// 描画(純粋仮想関数)
-		virtual void Draw() = 0;
+		// 更新(仮想関数)
+		virtual void Update() { for (auto& s : sprites) { s.second->Update(); } }
+		// 描画(仮想関数)
+		virtual void Draw() { for (auto& s : sprites) { s.second->Draw(); } }
 	};
 }
